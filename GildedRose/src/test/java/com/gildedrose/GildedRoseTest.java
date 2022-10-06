3b0f367f34d9d2acee054c0e124ff1ce33b7f3e5
Julian63800 <pet.dog63800@gmail.com>
2022-10-06 12:32:33 +0200

    All tests working. Finished !

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -24,13 +24,13 @@ class GildedRoseTest {
   }
 
   @Test
-  @DisplayName("Test that the quality is unchanged, sellIn negative value")
+  @DisplayName("Test that the quality is changed with sellIn negative value")
   void testQuality() {
-    Item element = new Item("foo", 0, 0);
+    Item element = new Item("foo", -1, 10);
     GildedRose app = new GildedRose(new Item[] {element});
     app.updateQuality();
-    assertThat(app.items[0].sellIn, is(-1));
-    assertThat(app.items[0].quality, is(0));
+    assertThat(app.items[0].sellIn, is(-2));
+    assertThat(app.items[0].quality, is(8));
   }
 
   @Test
@@ -91,6 +91,19 @@ class GildedRoseTest {
   }
 
   @Test
+  @DisplayName("Test that the Aged Brie quality is changed")
+  void testQualityAgedBrieInf0() {
+    Item element1 = new Item("Aged Brie", 0, 48);
+    Item element2 = new Item("Aged Brie", 0, 49);
+    GildedRose app = new GildedRose(new Item[] {element1, element2});
+    app.updateQuality();
+    assertThat(app.items[0].sellIn, is(-1));
+    assertThat(app.items[0].quality, is(50));
+    assertThat(app.items[1].sellIn, is(-1));
+    assertThat(app.items[1].quality, is(50));
+  }
+
+  @Test
   @DisplayName("Test Sulfuras is changed")
   void testItemSulfuras() {
     Item element = new Item("Sulfuras, Hand of Ragnaros", -1, 15);
@@ -105,10 +118,12 @@ class GildedRoseTest {
   void testBackstageItem() {
     Item element1 = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 0);
     Item element2 = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 0);
+    Item element1b = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49);
+    Item element2b = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49);
     Item element3 = new Item("Backstage passes to a TAFKAL80ETC concert", 13, 0);
     Item element4 = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 50);
 
-    GildedRose app = new GildedRose(new Item[] {element1, element2, element3, element4});
+    GildedRose app = new GildedRose(new Item[] {element1, element2, element3, element4, element1b, element2b});
 
     app.updateQuality();
     // Test for quality < 6
@@ -123,6 +138,16 @@ class GildedRoseTest {
     // Test for sellIn < 0
     assertThat(app.items[3].sellIn, is(-1));
     assertThat(app.items[3].quality, is(0));
+
+    // Test if quality > 50
+    // Test for quality < 6
+    assertThat(app.items[4].sellIn, is(4));
+    assertThat(app.items[4].quality, is(50));
+    // Test for quality < 11
+    assertThat(app.items[5].sellIn, is(9));
+    assertThat(app.items[5].quality, is(50));
+
+
   }
 
 }
